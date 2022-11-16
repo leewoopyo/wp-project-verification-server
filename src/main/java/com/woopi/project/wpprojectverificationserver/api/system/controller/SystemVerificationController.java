f97c1fbeea8929d616066ddec0cf7c18cd161ace
Lstamp <lozenkrantz@gmail.com>
2022-11-17 01:14:36 +0900

    java-version 출력 API 의 CORS 처리, 및 json으로 받을 수 있게 처리

--- src/main/java/com/woopi/project/wpprojectverificationserver/api/system/controller/SystemVerificationController.java
+++ src/main/java/com/woopi/project/wpprojectverificationserver/api/system/controller/SystemVerificationController.java
@@ -1,9 +1,15 @@
 package com.woopi.project.wpprojectverificationserver.api.system.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +24,17 @@ public class SystemVerificationController {
     Environment env;
 
     @GetMapping("/java-version")
-    public String javaVersion() {
+    public ResponseEntity<Map<String,String>> javaVersion(HttpServletResponse response) {
         String version = System.getProperty("java.version");
         log.debug("JAVA Version : {}", version);
+
+        response.addHeader("Access-Control-Allow-Origin", "*");
+
+        Map<String, String> data = new HashMap<String, String>();
+        data.put("java-version", version);
         
-        return "JAVA Version : " + version;
+        
+        return ResponseEntity.ok().body(data);
     }
 
     @GetMapping("/env")
